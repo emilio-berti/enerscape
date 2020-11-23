8b89747482e8cee164126d8215b01345900bf2f8
emilio-berti <emilio.berti90@gmail.com>
2020-11-23 14:54:44 +0100

    Data bugged

--- R/data.R
+++
@@ -1,5 +0,0 @@
-#' Digital elevation model of the Sirente-Velino Regional Park
-#'
-#' @format A raster layer with digital elevation data (meters)
-#' @source \url{http://tinitaly.pi.ingv.it/}
-"dem"
